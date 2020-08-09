3068fde006a9110511cfbce08ab89f6beae24540
ankit.vishnoi <ankit.vishnoi@servicenow.com>
2020-08-10 03:08:45 +0530

    Updates

---
+++ precache-manifest.b1c449f8c569e24bdc459be4de93d194.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "637ff43a4d41f325ac0416032ce64db7",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "36a8df2c08e2ad9f8270",
+    "url": "/covid19/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "5520c6fa9e1cd954ff67",
+    "url": "/covid19/static/css/main.91520a65.chunk.css"
+  },
+  {
+    "revision": "36a8df2c08e2ad9f8270",
+    "url": "/covid19/static/js/2.64aef8fd.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid19/static/js/2.64aef8fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5520c6fa9e1cd954ff67",
+    "url": "/covid19/static/js/main.ab7bd85d.chunk.js"
+  },
+  {
+    "revision": "7d1038710bb674affb6b",
+    "url": "/covid19/static/js/runtime-main.367779e9.js"
+  }
+]);
